9b18c9d4871845b77b8af23a7125dcf7c6756f1a
Anna Korunska <akorunska@gmail.com>
2019-03-23 14:57:38 +0200

    connecting to db in api

--- backend/api.py
+++ backend/api.py
@@ -1,13 +1,27 @@
 import json
 from flask_restful import Resource, Api, reqparse
-from flask import Flask, request
-
+from flask import Flask, Response
 from settings import API_HOST, API_PORT
-from routes import Users
+from pymongo import MongoClient
+from credentials import username, password
 
 
 app = Flask(__name__)
 api = Api(app)
+
+
+client = MongoClient('mongodb://%s:%s@ds121636.mlab.com:21636/amour_auth' % (username, password))
+db = client['amour_auth']
+
+
+class Users(Resource):
+    def get(self):
+        return Response(
+            response='',
+            status=200,
+            mimetype='application/json'
+)
+
 
 api.add_resource(Users, '/users', methods=['GET'])
 
