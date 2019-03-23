18cc558e5bd74a19f7b060b9635c3ece07ec3ed1
Anna Korunska <akorunska@gmail.com>
2019-03-23 17:03:20 +0200

    get and post request for /user

--- backend/api.py
+++ backend/api.py
@@ -1,29 +1,53 @@
+import codecs
 import json
-from flask_restful import Resource, Api, reqparse
+from flask_restful import Resource, Api, reqparse, request
 from flask import Flask, Response
 from settings import API_HOST, API_PORT
 from pymongo import MongoClient
 from credentials import username, password
 
+from database.users import User
 
 app = Flask(__name__)
 api = Api(app)
 
 
-client = MongoClient('mongodb://%s:%s@ds121636.mlab.com:21636/amour_auth' % (username, password))
-db = client['amour_auth']
-
-
 class Users(Resource):
     def get(self):
+        users = User.objects()
+        print(users)
         return Response(
-            response='',
+            response=users.to_json(),
             status=200,
             mimetype='application/json'
-)
+        )
+
+    def post(self):
+        user_data = json.loads(codecs.decode(request.data))
+        try:
+            user = User(
+                login=user_data['login'],
+                passport_id=user_data['passport_id'],
+                password=user_data['password'],
+                email=user_data['email'],
+                name=user_data['name'],
+                surname=user_data['surname'])
+            user.save()
+        except Exception as e:
+            return Response(
+                response=json.dumps({'error': e}),
+                status=400,
+                mimetype='application/json'
+            )
+
+        return Response(
+            response=json.dumps({'created': True}),
+            status=200,
+            mimetype='application/json'
+        )
 
 
-api.add_resource(Users, '/users', methods=['GET'])
+api.add_resource(Users, '/users', methods=['GET', 'POST'])
 
 
 if __name__ == '__main__':
--- backend/database/users.py
+++ backend/database/users.py
@@ -1,12 +1,30 @@
+from mongoengine import *
+from credentials import username, password
+
+# connect('mongodb://%s:%s@ds121636.mlab.com:21636/amour_auth' % (username, password))
+
+connect(
+    db='test',
+    username='user',
+    password='12345',
+    host='mongodb://%s:%s@ds121636.mlab.com:21636/amour_auth' % (username, password)
+)
 
 
-class UsersScheme():
-    def get_all(self):
-        pass
+class User(Document):
+    meta = {
+        'collection': 'users'
+    }
 
-    def get_by_id(self, db):
-        pass
+    login = StringField(max_length=20, unique=True, required=True)
+    passport_id = StringField(min_length=8, max_length=8, unique=True, required=True)
+    email = EmailField(unique=True)
+    password = StringField(max_length=500, required=True)
+    name = StringField(max_length=200, required=True)
+    surname = StringField(max_length=200, required=True)
 
-    def create(self, db):
-        pass
+
+# if __name__ == "__main__":
+#     user = User(login='bobbob', passport_id='ТТ567890', password='uiui',email='bob@example.com', name='Bob', surname='White')
+#     user.save()
 
